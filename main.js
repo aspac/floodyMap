3524cab9a3af47ab7a43e1b852b69ec1792f35c8
dana satriya <dana_satriya@yahoo.com>
2015-03-25 22:08:52 +0100

    remove depracated files and cleanup

--- main.js
+++ main.js
@@ -7,16 +7,16 @@
 // @return                
 //--------------------------------------------------
 function Main() {
-   utilsreadTextFile("http://localhost:8000/result.csv");
+ utilsreadTextFile("http://localhost:8000/result.txt");
 } 
 
 
-function displayPopupDummy(coords){
+function displayMap(coords){
 
   map = new OpenLayers.Map("mapdiv");
   map.addLayer(new OpenLayers.Layer.OSM());
 
-  var zoom=11;
+  var zoom=12;
 
   var grapHeight = 25
   var graphWidth = 21
@@ -38,45 +38,45 @@ function displayPopupDummy(coords){
   var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
 
 
-  for (var i = 0; i < restaurant_num; i++) {
+  for (var i = 1; i < restaurant_num; i++) {
 
     var feature = new OpenLayers.Feature.Vector(
-      new OpenLayers.Geometry.Point(coords[i].lng(), coords[i].lat() ).transform(epsg4326, projectTo),
-      {description: 'This is the value of<br>the description attribute'} ,
+      new OpenLayers.Geometry.Point(utilsgetComponent(coords,i,2), utilsgetComponent(coords,i,3)).transform(epsg4326, projectTo),
+      {description: utilsgetComponent(coords,i,0)} ,
       {externalGraphic: 'img/marker.png', graphicHeight: grapHeight, graphicWidth: graphWidth, graphicXOffset: graphXoffset, graphicYOffset: graphYoffset  }
       );    
 
     vectorLayer.addFeatures(feature);
   }
 
-    
-    map.addLayer(vectorLayer);
 
-    var controls = {
-      selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
-    };
+  map.addLayer(vectorLayer);
 
-    function createPopup(feature) {
-      feature.popup = new OpenLayers.Popup.FramedCloud("pop",
-        feature.geometry.getBounds().getCenterLonLat(),
-        null,
-        '<div class="markerContent">'+feature.attributes.description+'</div>',
-        null,
-        true,
-        function() { controls['selector'].unselectAll(); }
-        );
-      map.addPopup(feature.popup);
-    }
+  var controls = {
+    selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
+  };
 
-    function destroyPopup(feature) {
-      feature.popup.destroy();
-      feature.popup = null;
-    }
-    
-    map.addControl(controls['selector']);
-    controls['selector'].activate();
-
+  function createPopup(feature) {
+    feature.popup = new OpenLayers.Popup.FramedCloud("pop",
+      feature.geometry.getBounds().getCenterLonLat(),
+      null,
+      '<div class="markerContent">'+feature.attributes.description+'</div>',
+      null,
+      true,
+      function() { controls['selector'].unselectAll(); }
+      );
+    map.addPopup(feature.popup);
   }
+
+  function destroyPopup(feature) {
+    feature.popup.destroy();
+    feature.popup = null;
+  }
+
+  map.addControl(controls['selector']);
+  controls['selector'].activate();
+
+}
 
 /*===========================================================
 // 
@@ -105,8 +105,6 @@ http://jsfiddle.net/vHKYH/
 */
 function processData(str){
 
-  alert("..sPROCESSING DATA..")
-
   var arr = [];
   var quote = false; 
 
@@ -123,136 +121,38 @@ function processData(str){
       arr[row][col] += cc;
     }
 
-   if (arr.length == 0) {
-    alert("No address defined!");
-    return;
-  }
+    if (arr.length == 0) {
+      alert("No address defined!");
+      return;
+    }
 
-  var last_arr = arr.length;
-  last_arr -=1;
-  var pos = [];
-  
-  var back = 0;
-  alert("START..")
-  while (back != 1) {
-    back = utilsrequestcoord(arr, last_arr, pos);
-  }
-  alert("DONE ..")
-  
-  /*
-  for (var i = 1; i < last_arr; i++) {
-
-    var currAddress = arr[i];
-    var currAddress = currAddress[1]
     
-    sleep(200);  
-    var geocoder = new google.maps.Geocoder();
-    //alert("got currAddress of " +currAddress)
+  //probe :   var component_displayed = utilsgetComponent(arr,1,0)
 
-    geocoder.geocode({'address':currAddress}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        //debug: 
-        //alert("debug!")
-        //alert(results[0].geometry.location.lat());
-        //var longitude = results[0].geometry.location.lng();
-        pos.push(results[0].geometry.location);
-        if (pos.length == 5) {
-          alert("display Dummy!!!!!!")
-          //displayMarkers(pos);
-          displayPopupDummy(pos);
-        } 
-      } 
-      else {
-        alert("hickup status from GOOGLE " + status + "where request is " + i)
-        return;
-      }
-    });
-  }
-  */
+  displayMap(arr);
+
+
 }
 
 
-
-
 //--------------------------------------------------
-// Function that returns address or name of place
-//          depending on the caller
-// @param :     o       ...... address object
-//              idx     ...... iteration index
-//              type    ...... 0 for address, 1 for name             
+// Function that returns component of the array taken from text file, 
+// that is separated by (,)
+// @param :     arrayvalue       ...... array value to be split
+//                               ...... this consist of name,address,lon,lat
+//              arrayidx         ...... index of array to be fetched
+//              type             ...... 0 for name,
+//                                      1 for address,
+//                                      2 for lon,
+//                                      3 for lat             
 // @return                
 //--------------------------------------------------
 
-function utilsgetAddressName(o, idx, type){
-  var out = '';
-  var ctr = 0;
+function utilsgetComponent(arrayvalue, arrayidx, type){
 
-  for (var p in o) {
+ var processedtxt = arrayvalue[arrayidx].toString().split(",");
 
-    //out += p + '\n';
-    if (idx == ctr) {
-     if (is_address == type) {
-      out += p;
-    } else {
-      out += o[p];
-    }
-    break;
-  }
-
-  ctr++;
-}
-
-return out;
+ return processedtxt[type];
 
 }
 
-//--------------------------------------------------
-// Sleep
-//          
-// @param :     milliseconds       ...... address object
-// @return                
-//--------------------------------------------------
-function sleep(milliseconds) {
-  var start = ""
-  start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
-
-function utilsrequestcoord(arr, last_arr, pos){
-
-   var back = 0;
-   for (var i = 1; i < last_arr; i++) {
-
-    var currAddress = arr[i];
-    var currAddress = currAddress[1]
-    
-    var geocoder = new google.maps.Geocoder();
-    //alert("got currAddress of " +currAddress)
-    sleep(1500);
-    geocoder.geocode({'address':currAddress}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        //debug: 
-        //alert("debug!")
-        //alert(results[0].geometry.location.lat());
-        //var longitude = results[0].geometry.location.lng();
-        pos.push(results[0].geometry.location);
-        if (pos.length == 6) {
-          //displayMarkers(pos);
-          displayPopupDummy(pos);
-        } 
-      } 
-      else {
-        //alert("hickup status from GOOGLE " + status + "where request is " + i)
-        sleep(2000);
-      }
-    });
-  }
-   
-  back = 1;
-  return back;
-}
